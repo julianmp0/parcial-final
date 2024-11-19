a952d0653bafe4b169a7dfbf9162fe4d76b8a59b
Julian Martinez <jcmartinezpi@falabella.com.co>
2024-11-19 14:57:19 -0500

    initial project

---
+++ src/test/java/unisangil/parcialfinal/ParcialFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package unisangil.parcialfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
